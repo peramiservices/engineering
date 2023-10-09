d7b3da9470b4c01a7b3639cf4474321099ef3e6c
peramiservices <145301429+peramiservices@users.noreply.github.com>
2023-10-10 07:20:23 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    number: document.getElementById("number").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_0vepg6n";
+  const templateID = "template_wgfyvx9";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("number").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
+
