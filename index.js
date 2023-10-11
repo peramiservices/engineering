0eb5d195ec2a491a2870291581d4f9c0f2e85458
peramiservices <145301429+peramiservices@users.noreply.github.com>
2023-10-11 09:33:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,22 +4,20 @@ function sendMail() {
     email: document.getElementById("email").value,
     number: document.getElementById("number").value,
     message: document.getElementById("message").value,
+    currentDate: new Date().toLocaleDateString()  // Add current date to the params object
   };
 
   const serviceID = "service_0vepg6n";
   const templateID = "template_wgfyvx9";
 
-    emailjs.send(serviceID, templateID, params)
-    .then(res=>{
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("number").value = "";
-        document.getElementById("message").value = "";
-        console.log(res);
-        alert("Your message sent successfully!!")
-
+  emailjs.send(serviceID, templateID, params)
+    .then(res => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("number").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message sent successfully!!");
     })
-    .catch(err=>console.log(err));
-
+    .catch(err => console.log(err));
 }
-
